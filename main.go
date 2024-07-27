b65b1317a880f41bb5410e38a38f59cd52bf157c
immodi <ay727467@gmail.com>
2024-07-27 09:28:44 +0300

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	println("Currently Listening on http://localhost:8080....")
+	r.Run()
+}
